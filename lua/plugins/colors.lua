11d03d9c09e3ee52788bad4c0e57f5b2b946876b
Scott Bennett <scottbennett027@gmail.com>
2025-03-25 21:42:45 -0400

    New

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {
+      style = "night",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "csv",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "json5",
+        "lua",
+        "matlab",
+        "proto",
+        "python",
+        "regex",
+        "toml",
+        "yaml",
+      },
+    },
+  },
+}
