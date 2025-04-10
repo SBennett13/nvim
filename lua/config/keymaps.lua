5b7ba2cc3614fc96324a81d688aa293f34009d9d
Scott Bennett <scottbennett027@gmail.com>
2025-04-09 23:05:58 -0400

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- Center after up and down
+keymap.set("n", "<C-d>", "<C-d>nn")
+keymap.set("n", "<C-u>", "<C-u>nn")
+
+keymap.set("n", "n", "nzz")
+keymap.set("n", "N", "Nzz")
