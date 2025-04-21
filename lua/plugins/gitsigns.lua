dc8a9668152c1e39fe0ab0978e96397aeef6b07d
Scott Bennett <40250637+SBennett13@users.noreply.github.com>
2025-04-21 15:11:32 -0400

    Create gitsigns.lua

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsigns = require("gitsigns")
+      gitsigns.setup({
+          current_line_blame = true
+      })
+    end
+    event = { "BufReadPre", "BufNewFile" },
+  }
+}
